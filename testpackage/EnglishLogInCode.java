ad512a73f3337b50209a1bb56d0db4cf5ae27442
NGBlock <108145237+NGBlock@users.noreply.github.com>
2024-02-20 20:46:16 +0100

    test

---
+++ testpackage/EnglishLogInCode.java
@@ -0,0 +1,17 @@
+package testpackage;
+
+import java.util.Scanner;
+
+
+public class EnglishLogInCode {
+
+
+
+    // Intro Method of the English version of the App.
+    public static void StartupLoginEnglish() {
+
+        System.out.println("hi");
+
+
+    }
+    }
